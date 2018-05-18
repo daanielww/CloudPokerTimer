f9d8a0c115f348a9b90050d5a9e2a9f44dfcc26c
Daniel Wang <daniel.wang.1999@hotmail.com>
2018-05-18 11:44:10 -0400

    database functionaility

--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type blindStucture struct {
+	Small    int64
+	Big      int64
+	Ante     int64
+	Level    int64
+	Name     string
+	Duration int64
+}
+
+type UserGame struct {
+	ID        bson.ObjectId `bson:"_id,omitempty"`
+	Level     int64
+	UserID    string
+	Start     int64
+	State     bool
+	Remain    int64
+	structure blindStucture
+}
 
 func main() {
-	fmt.Println("Hello, emailtopia!")
+	session, err := mgo.Dial("localhost") // connect to server
+	if err != nil {
+		log.Fatal("cannot dial mongo", err)
+	}
+	defer session.Close() // close the connection when main returns
+
+	collection := session.DB("game").C("userGame") //make the collection
+
+	bs := blindStucture{
+		Small:    10,
+		Big:      11,
+		Ante:     12,
+		Level:    13,
+		Name:     "Daniel",
+		Duration: 24,
+	}
+
+	user := UserGame{
+
+		Level:     123,
+		UserID:    "asdasdasd",
+		Start:     2222,
+		State:     true,
+		Remain:    123123,
+		structure: bs,
+	}
+
+	err = collection.Insert(user)
+
 }
