54b28c5db0cdcefade96a0a34759588f4364f0be
Henry Lach <hlach@emailtopia.com>
2018-05-18 16:16:10 -0400

    fff

--- timer.go
+++ timer.go
@@ -13,7 +13,18 @@ func calculateCurrentGameTime(game) {
 	accumulatePauseDuration := game.accumulatePauseDuration
 	paused := game.Paused
 	gameElapsedTime := (currentTime - startTime)
-	gameBlindStructure := game.BlindStucture
-	if 
+	gameBlindStructureLevels := game.BlindStucture.AllLevels[]
+	 
+	if paused = true {
+		accumulatePauseDuration += (currentTime - currentPausedStartTime)
+	}
+
+	gameDuration := gameElapsedTime - accumulatePauseDuration
+
+	func main() {  
+		for i := 1; i <= 10; i++ {
+			fmt.Printf(" %d",i)
+		}
+	}
 
 }
