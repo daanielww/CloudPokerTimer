46dced5a5f079824c03888fb1c5f4f954221e336
Charvi <cshah@emailtopia.com>
2018-05-24 16:33:05 -0400

    make game data in db

--- Game.go
+++ Game.go
@@ -91,34 +91,7 @@ func games(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	row1 := row{
-		SmallBlind: 5,
-		BigBlind:   10,
-		Ante:       0,
-		Level:      1,
-		Duration:   11,
-	}
-
-	row2 := row{
-		SmallBlind: 10,
-		BigBlind:   20,
-		Ante:       0,
-		Level:      2,
-		Duration:   12,
-	}
-
-	userGame := UserGame{
-		User:                      "Sam",
-		StartTime:                 time.Now(),
-		Paused:                    false,
-		CurrentPausedStartTime:    time.Now(),
-		CurrentLevelTime:          555,
-		CurrentLevel:              1,
-		BlindScheduleName:         "Office Turbo",
-		Levels:                    []row{row1, row2},
-		CurrentPausedTime:         time.Now(),
-		AccumulatedPausedDuration: 1213,
-	}
+	userGame := makeDummyData()
 
 	db.C("gameInfo").Insert(userGame)
 
@@ -171,4 +144,43 @@ func update(w http.ResponseWriter, r *http.Request) {
 	}
 
 	db.C("gameInfo").Update(bson.M{"User": game.User}, bson.M{"$set": bson.M{"Paused": u.Status}})
+}
+
+func makeDummyData() UserGame {
+
+	smallBlindArray := []int64{5, 10, 25, 75, 100, 150, 200, 300, 400, 500, 700, 1000, 1500, 2000, 3000}
+	bigBlindArray := []int64{10, 20, 50, 100, 150, 200, 300, 400, 600, 800, 1000, 1400, 2000, 3000, 4000, 6000}
+	anteArray := []int64{0,0,5,10,10,25,25,25,50,50,100,100,200,300,400,600}
+	rows := []row{}
+	rowOb := row{}
+
+
+
+	for i := 0; i < len(smallBlindArray); i++ {
+
+		rowOb = row {
+			SmallBlind:		smallBlindArray[i],
+			BigBlind:		bigBlindArray[i],
+			Ante:			anteArray[i],
+			Level:			int64(i + 1),
+			Duration:		int64(7),
+		}
+
+		rows = append(rows, rowOb)
+	}
+
+	userGame := UserGame{
+		User:                      "Sam",
+		StartTime:                 time.Now(),
+		Paused:                    false,
+		CurrentPausedStartTime:    time.Now(),
+		CurrentLevelTime:          555,
+		CurrentLevel:              1,
+		BlindScheduleName:         "Office Turbo",
+		Levels:                    rows,
+		CurrentPausedTime:         time.Now(),
+		AccumulatedPausedDuration: 1213,
+	}
+
+	return userGame
 }
